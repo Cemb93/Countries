5d0c0cc1ed7127b8f58286dab31b967ab8b7688f
Cemb93 <karloz13m@gmail.com>
2023-01-13 00:35:08 -0500

    algunos ajustes en detail

--- api/src/servers/update_Activity.js
+++ api/src/servers/update_Activity.js
@@ -17,3 +17,37 @@ const update_activity = async (req, res) => {
 }
 
 module.exports = { update_activity }
+
+// const update_activity = async (req, res) => {
+//   const { id } = req.params;
+//   const { name, duration, difficulty, season, nameCountry } = req.body;
+//   try {
+//     if (
+//       !name ||
+//       name === '' ||
+//       (!difficulty && isNaN(difficulty) == true) ||
+//       (!duration && isNaN(duration) == true) ||
+//       (
+//         season !== 'Verano' &&
+//         season !== 'Otoño' &&
+//         season !== 'Invierno' &&
+//         season !== 'Primavera' ||
+//         nameCountry.length === 0
+//       )
+//     ) {
+//       return res.status(404).json('la actividad no se ah modificado');
+//     } else {
+//       const current = await Activity.update(
+//         { name, duration, difficulty, season },
+//         {
+//           where: { id, },
+//         }
+//       );
+//       console.log('CURRENT:', current);
+//       return res.json(current)
+//     }
+//   } catch (error) {
+//     return res.json({ error: error.message });
+//   }
+// };
+// module.exports = { update_activity }
--- client/src/components/Details/Details.jsx
+++ client/src/components/Details/Details.jsx
@@ -20,68 +20,69 @@ export const Details = (props) => {
     <div className={s.div_principal}>
       {!detail.error ? (
         <div className={s.div_detail}>
-          <p>
-            <strong>Código de País: </strong>
-            {detail.id}
-          </p>
-          <p>
-            <strong>País: </strong>
-            {detail.name}
-          </p>
-          <img src={detail.flags} alt="Img Not Founf" />
-          <p>
-            <strong>Continente: </strong>
-            {detail.continents}
-          </p>
-          <p>
-            <strong>Capital: </strong>
-            {detail.capital}
-          </p>
-          <p>
-            <strong>Sub-Región: </strong>
-            {detail.subregion}
-          </p>
-          <p>
-            <strong>Área: </strong>
-            {detail.area} km2
-          </p>
-          <p>
-            <strong>Población: </strong>
-            {detail.population} Hab.
-          </p>
-          <h3>Actividad(es) Turística(s):</h3>
-          <div className={s.div_all_activities}>
-            {detail.activities?.map((el) => {
-              return (
-                <div className={s.div_activity}>
-                  <p>
-                    <strong>Nombre de la Actividad: </strong>
-                    {el.name}
-                  </p>
-                  <p>
-                    <strong>Dificultad: </strong>
-                    Nivel - {el.difficulty}
-                  </p>
-                  <p>
-                    <strong>Duración: </strong>
-                    {el.duration} hora(s)
-                  </p>
-                  <p>
-                    <strong>Temporada: </strong>
-                    {el.season}
-                  </p>
-                  <button 
-                    onClick={() => {
-                      dispatch(delete_activity(el.id));
-                      history.go(0);
-                    }}
-                  >
-                    ELIMINAR ACTIVIDAD
-                  </button>
-                  <Link to={`/edit-activity/${el.id}`} >Editar Actividad</Link>
-                </div>
-              );
-            })}
+          <div className={s.div_date_country} >
+            <p>
+              <strong>País: </strong>
+              {detail.name}
+            </p>
+            <img src={detail.flags} alt="Img Not Founf" />
+            <p>
+              <strong>Continente: </strong>
+              {detail.continents}
+            </p>
+            <p>
+              <strong>Capital: </strong>
+              {detail.capital}
+            </p>
+            <p>
+              <strong>Sub-Región: </strong>
+              {detail.subregion}
+            </p>
+            <p>
+              <strong>Área: </strong>
+              {detail.area} km2
+            </p>
+            <p>
+              <strong>Población: </strong>
+              {detail.population} Hab.
+            </p>
+          </div>
+          <div className={s.div_each_activity} >
+            <h3>Actividad(es) Turística(s):</h3>
+            <div className={s.div_all_activities}>
+              {detail.activities?.map((el) => {
+                return (
+                  <div className={s.div_activity}>
+                    <p>
+                      <strong>Nombre de la Actividad: </strong>
+                      {el.name}
+                    </p>
+                    <p>
+                      <strong>Dificultad: </strong>
+                      Nivel - {el.difficulty}
+                    </p>
+                    <p>
+                      <strong>Duración: </strong>
+                      {el.duration} hora(s)
+                    </p>
+                    <p>
+                      <strong>Temporada: </strong>
+                      {el.season}
+                    </p>
+                    <button
+                      className={s.btn_delete}
+                      onClick={() => {
+                        dispatch(delete_activity(el.id));
+                        history.go(0);
+                      }}
+                    >
+                      ELIMINAR ACTIVIDAD
+                    </button>
+                    {/* <Link to={`/edit-activity/${el.id}`} >Editar Actividad</Link> */}
+                  </div>
+                );
+              })}
+            </div>
           </div>
           <Link to={"/home"}>
             <button className={s.btn}>Regresar</button>
