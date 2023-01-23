9ae2976006cce67cd1a01aa186f1f35f6615f058
Cemb93 <karloz13m@gmail.com>
2023-01-22 19:24:23 -0500

    style in activities

--- client/src/components/All_Activities/All_Activities.jsx
+++ client/src/components/All_Activities/All_Activities.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import { delete_activity, get_all_activities } from '../../redux/actions';
-import s from './Activities.module.css';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { delete_activity, get_all_activities } from "../../redux/actions";
+import s from "./Activities.module.css";
 
 export const All_Activities = () => {
   const dispatch = useDispatch();
@@ -11,39 +11,56 @@ export const All_Activities = () => {
   useEffect(() => {
     dispatch(get_all_activities());
   }, [dispatch]);
-  
+
   let { activities } = useSelector((state) => state);
-  
+
   const handleClick = (id, name) => {
     dispatch(delete_activity(id));
     dispatch(get_all_activities());
     alert(`La Actividad: ${name.toUpperCase()}, fue eliminada`);
-  }
+  };
 
   const handleReturn = () => {
-    history.push('/home');
-  }
+    history.push("/home");
+  };
 
   return (
-    <div className={s.container} >
-      <div className={s.activities} >
-        {
-          activities?.map(el => {
-            return (
-              <div>
-                <div key={el.id} className={s.activity} >
-                  <p><b>Actividad: </b>{el.name}</p>
-                  <p><b>Duración: </b>{el.duration}</p>
-                  <p><b>Dificultad: </b>{el.difficulty}</p>
-                  <p><b>Temporada: </b>{el.season}</p>
-                  <button onClick={() => handleClick(el.id, el.name)} >ELIMINAR</button>
-                </div>
+    <div className={s.container}>
+      <div className={s.activities}>
+        {activities?.map((el) => {
+          return (
+            <div>
+              <div key={el.id} className={s.activity}>
+                <p>
+                  <b>Actividad: </b>
+                  {el.name}
+                </p>
+                <p>
+                  <b>Duración: </b>
+                  {el.duration}
+                </p>
+                <p>
+                  <b>Dificultad: </b>
+                  {el.difficulty}
+                </p>
+                <p>
+                  <b>Temporada: </b>
+                  {el.season}
+                </p>
+                <button
+                  onClick={() => handleClick(el.id, el.name)}
+                  className={s.btn_delete}
+                >
+                  ELIMINAR
+                </button>
               </div>
-            );
-          })
-        }
+            </div>
+          );
+        })}
       </div>
-      <button onClick={handleReturn} >Regresar</button>
+      <button onClick={handleReturn} className={s.btn_return}>
+        Regresar
+      </button>
     </div>
-  )
-}
+  );
+};
